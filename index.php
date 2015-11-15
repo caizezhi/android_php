a7a6aeee9d5eb3751b08c7669818179a88ff5718
CAI <caizezhi@gmail.com>
2015-11-15 21:38:39 +0800

    update downloadPic_test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'Slim/Slim.php';
 require 'function.php';
 $app = new Slim();
 //index
-$app->get("/:lesson/:teacher/:user/:is_puiblic","lessons");
+$app->get("/:lesson/:teacher/:user/:is_public","lessons");
 $app->post("/uploadPic","uploadPic");
 $app->post("/uploadVoi","uploadVoi");
 $app->post("/createlesson","createLesson");
@@ -13,7 +13,7 @@ $app->post("/updategrade","updategrade");
 $app->get("/:id_student/:id_lesson","get_grade");
 $app->post("/test_json","get_info");
 $app->post("/downloadPic","downloadPic");
-$app->post("/:userId/:lessonName/:is_public","downloadPic_test");
+$app->get("/:userId/:lessonName/:is_public","downloadPic_test");
 //admin
 $app->post("/admin/login","login");
 $app->post("/admin/register","register");
